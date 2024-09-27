d94af0fef312e2740117125b9b40dd0ab98acd69
emmacsmith <emmacsmith45@hotmail.co.uk>
2024-09-27 14:44:33 +0100

    emmas-webpage-1

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log("Hello, World!");
+
+
+
+
+//creating my accessible reveal me button 
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", function (event) {
+    const isExpanded = button.getAttribute("aria-expanded")
+
+    if (isExpanded === "true"){
+        //hide the content 
+        button.setAttribute("aria-expanded", "false")
+        button.nextElementSibling.setAttribute("hidden", "")
+        button.nextElementSibling.nextElementSibling.setAttribute("hidden", "")
+
+    } else if (isExpanded === "false"){
+        //show the content
+        button.setAttribute("aria-expanded", "true")
+        button.nextElementSibling.removeAttribute("hidden")
+        button.nextElementSibling.nextElementSibling.removeAttribute("hidden")
+    }
+})
+  
